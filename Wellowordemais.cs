f0f8fa073f76d896d08278d25f98ba00b9d8c6f7
Joãonuvem <joao.rocha53396@alunos.ufersa.edu.br>
2024-10-22 08:56:05 -0300

    Create Wellowordemais.cs

---
+++ Wellowordemais.cs
@@ -0,0 +1,17 @@
+   /* 
+   primeiro código em c#
+   
+   autor: João Carlos de Sousa Gurgel Rocha.
+
+   GitHub: Joaonuvem / Joãonuvem.
+
+   link: https://github.com/Joaonuvem 
+
+   O código exibe algumas frases em c#.
+*/
+Console.WriteLine("Hello Wrrld!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote yout vai ti tomar no chu porra");
+Console.WriteLine("COngratularion! ");
+Console.Write("Você é um desocupado!!!");
